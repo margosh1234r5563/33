ce100a34564f0856a969c1c12c805cea6ecbb7e5
margosh1234r5563 <m.rusakova@g.nsu.ru>
2024-02-07 21:52:51 +0700

    Hello Minac

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import turtle
+def trg(x,y,a):
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.down()
+    turtle.right(60)
+    turtle.forward(a)
+    turtle.right(60)
+    turtle.forward(a)
+    turtle.right(60)
+    turtle.forward(a)
+trg(0,0,180)
